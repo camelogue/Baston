5a248c3b758e66113dd64a484e377bf3eaf17277
Beste <83185578+bestesengul@users.noreply.github.com>
2023-03-23 23:25:02 +0300

    Gallery replaced by Magnifier

--- app/src/main/java/com/example/baston/ui/magnifier/MagnifierFragment.java
+++ app/src/main/java/com/example/baston/ui/magnifier/MagnifierFragment.java
@@ -1,4 +1,4 @@
-package com.example.baston.ui.gallery;
+package com.example.baston.ui.magnifier;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,21 +10,21 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.baston.databinding.FragmentGalleryBinding;
+import com.example.baston.databinding.FragmentMagnifierBinding;
 
-public class GalleryFragment extends Fragment {
+public class MagnifierFragment extends Fragment {
 
-    private FragmentGalleryBinding binding;
+    private FragmentMagnifierBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        GalleryViewModel galleryViewModel =
-                new ViewModelProvider(this).get(GalleryViewModel.class);
+        MagnifierViewModel galleryViewModel =
+                new ViewModelProvider(this).get(MagnifierViewModel.class);
 
-        binding = FragmentGalleryBinding.inflate(inflater, container, false);
+        binding = FragmentMagnifierBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textGallery;
+        final TextView textView = binding.textMagnifier;
         galleryViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
--- app/src/main/java/com/example/baston/ui/magnifier/MagnifierViewModel.java
+++ app/src/main/java/com/example/baston/ui/magnifier/MagnifierViewModel.java
@@ -1,16 +1,16 @@
-package com.example.baston.ui.gallery;
+package com.example.baston.ui.magnifier;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class GalleryViewModel extends ViewModel {
+public class MagnifierViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public GalleryViewModel() {
+    public MagnifierViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is gallery fragment");
+        mText.setValue("BUYUTEC SAYFASI");
     }
 
     public LiveData<String> getText() {
